060b88576ed1272c7c447657974fdfa00c09a3be
ReginaPaz <125509895+Pazregina19@users.noreply.github.com>
2026-05-26 18:47:45 +0100

    bugfix:node modules

--- frontend/supermarket-app/src/app/dashboard/admin-dashboard/admin-dashboard.ts
+++ frontend/supermarket-app/src/app/dashboard/admin-dashboard/admin-dashboard.ts
@@ -1,9 +1,92 @@
-import { Component } from '@angular/core';
+import {Component,OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SupermarketService} from '../../services/supermarket';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+
+  standalone: true,
+
+  imports: [
+    CommonModule
+  ],
+
   templateUrl: './admin-dashboard.html',
-  styleUrl: './admin-dashboard.css',
+
+  styleUrls: ['./admin-dashboard.css']
 })
-export class AdminDashboard {}
+export class AdminDashboard
+implements OnInit {
+
+  pendingSupermarkets: any[] = [];
+
+  loading: boolean = true;
+
+  constructor(
+    private supermarketService:
+    SupermarketService
+  ) {}
+
+  ngOnInit(): void {
+
+    this.loadPendingSupermarkets();
+
+  }
+
+  loadPendingSupermarkets(): void {
+
+    this.supermarketService
+      .getAll()
+      .subscribe({
+
+        next: (response: any) => {
+
+          this.pendingSupermarkets =
+            response.filter(
+              (supermarket: any) =>
+                supermarket.approved === false
+            );
+
+          this.loading = false;
+
+        },
+
+        error: (error: any) => {
+
+          console.error(error);
+
+          this.loading = false;
+
+        }
+
+      });
+
+  }
+
+  approveSupermarket(id: string): void {
+
+    this.supermarketService
+      .approve(id)
+      .subscribe({
+
+        next: () => {
+
+          this.pendingSupermarkets =
+            this.pendingSupermarkets.filter(
+              (s: any) =>
+                s._id !== id
+            );
+
+        },
+
+        error: (error: any) => {
+
+          console.error(error);
+
+        }
+
+      });
+
+  }
+
+}
